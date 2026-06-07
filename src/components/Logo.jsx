c503e226199488f48bcb4cb6a0ac8a2867764677
Taqui-Hassan <taquihassan6@gmail.com>
2026-06-07 23:59:50 +0530

    remove old root files

--- src/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import BlogWorks from './images/BlogWorks.webp'
-function Logo({width='100px'}) {
-  return (
-    <div><img src={BlogWorks} alt="" style={{width:width}}/></div>
-  )
-}
-
-export default Logo
